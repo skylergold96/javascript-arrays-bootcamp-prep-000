114da6d8de3c95b5bf05523a8dab086d50d6f408
Skyler H Gold <skylergold96@gmail.com>
2019-12-25 03:17:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,40 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray() {
-const array1 = [5];
-return array1.unshift(4);
+ 
+ 
+function destructivelyAddElementToBeginningOfArray(array, element ) {
+ return array.unshift(element);
 }
 
-function destructivelyAddElementToBeginningOfArray () {
- const array2 = [...array1,5];
- return array2;
-} 
-//Second function is not passing
+function addElementToBeginningOfArray (array,element){
+const array1 = [element,...array];
+return array1;
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+ return array.push(element);
+}
+
+function addElementToEndOfArray (array,element){
+const array1 = [...array, element];
+return array1;
+}
+
+function accessElementInArray (array,index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array, element) {
+  return array.shift(element)
+}
+function removeElementFromBeginningOfArray (array, element) {
+  const begArray = array.slice(element);
+  return begArray;
+}
+function destructivelyRemoveElementFromEndOfArray (array, element) {
+  return array.pop(element)
+}
+function removeElementFromEndOfArray (array, element) {
+  let lastArray = [array];
+  
+  
+}
